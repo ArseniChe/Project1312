81fe9a40742f6ff2292cf9707a7f701427aac6b2
Arseni <cherkasov.arseni@gmail.com>
2023-12-13 19:53:42 +0100

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person("Arseni", "Che");
+        System.out.println(person.info());
     }
 }
